ef5fc110b99ae534e921addce9ddc784df0e27db
BaekSoo <heech980120@gmail.com>
2021-07-31 14:21:58 +0900

    21.07.31

--- Gold Miner/1_gold miner.py
+++ Gold Miner/1_gold miner.py
@@ -1,6 +1,25 @@
 import os #파일 디렉터리를 다루기 위함
 import pygame
 
+
+#집게 클래스
+class Claw(pygame.sprite.Sprite):
+    def __init__(self, image, position):
+        super().__init__()
+        self.image = image
+        self.rect = image.get_rect(center = position)
+
+        self.offset = pygame.math.Vector2(default_offset_x_claw, 0)
+        self.position = position
+
+    def update(self):
+        rect_center = self.position + self.offset
+        self.rect = self.image.get_rect(center = rect_center)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        pygame.draw.line(screen, BLACK, self.position, self.rect.center, 5) #직선 그리기
+
 #보석 클래스
 class Gemstone(pygame.sprite.Sprite):
     def __init__(self, image, position):
@@ -29,6 +48,13 @@ pygame.display.set_caption("Gold Miner") #타이틀 이름설정
 
 clock = pygame.time.Clock()#프레임을 제한히기 위함
 
+#게임 관련 변수
+default_offset_x_claw = 40 #집게와 중심점 사이의 거리
+
+#색깔 변수
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
 #배경 이미비 불러오기
 current_path = os.path.dirname(__file__) #현재 파일의 위치 반환
 background = pygame.image.load(os.path.join(current_path, "background.png")) # 디렉터리와 디렉터리(파일명) 결합
@@ -45,6 +71,10 @@ gemstone_images = [
 gemstone_group = pygame.sprite.Group()
 setup_gemstone() #게임에 원하는 만큼의 보석을 정의
 
+#집게 
+claw_image = pygame.image.load(os.path.join(current_path, "claw.png"))
+claw = Claw(claw_image, (screen_width//2, 110)) #가로위치는 화면 크기 기준으로 절반, 세로 위치는 위에서 110 px
+
 running = True
 while running:
     clock.tick(30) #FPS 값이 30으로 고정
@@ -56,6 +86,8 @@ while running:
     screen.blit(background, (0,0))
 
     gemstone_group.draw(screen) #그룹 내 모든 스프라이트를 screen에 표시
+    claw.update()
+    claw.draw(screen)
 
     pygame.display.update() #display 업데이트
 
